9713b90ba5b620c12420b9c1e8a19c3093bb7b43
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-05-03 21:21:26 +0900

    first commit

---
+++ src/DuolingoTimer.js
@@ -0,0 +1,35 @@
+//プログレスバーを追加する場所を用意する
+async function addElementForProgressBar() {
+    const add = document.createElement("div");
+    add.setAttribute("id", "container")
+    const target = document.getElementsByClassName("o3j99 c93Gbe")[0];
+
+    target.before(add);
+    //cssを追加
+    add.style.margin = "20px";
+    add.style.width = '95%';
+    add.style.height = '8px';
+    return
+}
+
+async function addProgressBar() {
+    const bar = new ProgressBar.Line(container, {
+        strokeWidth: 4,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: { width: '100%', height: '100%' }
+    });
+    bar.animate(1);
+}
+
+async function main() {
+    await addElementForProgressBar();
+    await addProgressBar();
+}
+
+document.addEventListener("load",
+    main()
+)
