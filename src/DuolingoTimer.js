77374ecf178609372c8793832fdd9898c6cd3e15
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-05-04 20:03:28 +0900

    #9 animeteを使用

--- src/DuolingoTimer.js
+++ src/DuolingoTimer.js
@@ -15,38 +15,25 @@ async function addElementForProgressBar() {
 //プログレスバーを追加する
 async function addProgressBar() {
     //ユーザーが指定した秒数
-    let inputSeconds = 11;
+    let inputSeconds = 30;
 
     const bar = new ProgressBar.Line(container, {
-        strokeWidth: 4,
+        strokeWidth: 1,
         easing: 'easeInOut',
         duration: 1400,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
+        color: '#eee',
+        trailColor: '#FFEA82',
+        trailWidth: 4,
         svgStyle: { width: '100%', height: '100%' }
     });
-    bar.set(1);
+    bar.animate(-1, {
+        duration: inputSeconds * 1000,
+        easing: 'linear',
+    })
 
-    //引くべき数値
-    const calcSeconds = await calc(inputSeconds);
-    console.log(calcSeconds);
-
-    let count = 1;
     const interval = setInterval(() => {
-        count -= calcSeconds;
-        if (inputSeconds >= 0) {
-            bar.set(count);
-            inputSeconds -= 1;
-            console.log(count);
-        } else { clearInterval(interval); };
         console.log("ループ確認用")
     }, 1000);
-}
-
-//引くべき数字を求める
-async function calc(number) {
-    return Math.round((1 / number) * 100000) / 100000
 }
 
 async function main() {
