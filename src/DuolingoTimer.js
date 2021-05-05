3b3cc613e5943493b9fddf9af5ce8d8690c42537
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-05-05 10:03:02 +0900

    #13

--- src/DuolingoTimer.js
+++ src/DuolingoTimer.js
@@ -10,19 +10,47 @@ async function isSkillUrl() {
 }
 //1問につき一度だけ実行する
 async function addElementForProgressBar() {
-    const target = document.getElementsByClassName("mQ0GW")[0];
-    const add = document.createElement("div");
-    add.setAttribute("id", "container")
+    try {
+        document.querySelector('#container').remove();
+    } finally {
+        const target = document.getElementsByClassName("mQ0GW")[0];
+        const add = document.createElement("div");
+        add.setAttribute("id", "container")
 
-
-    target.before(add);
-    //cssを追加
-    add.style.margin = "1%";
-    add.style.width = '98%';
-    add.style.height = '8px';
-    add.style.top = '92%';
-    return
+        target.before(add);
+        //cssを追加
+        add.style.margin = "1%";
+        add.style.width = '98%';
+        add.style.height = '8px';
+        add.style.top = '92%';
+        return
+    }
 }
+
+//次の問題に行ったかどうかを判定する
+async function isNextQuestion() {
+    //要素が現れるまで待つ
+    const interval = setInterval(() => {
+        const target = document.querySelector('.jWza5');
+        if (target) {
+            let timer = null;
+
+            clearInterval(interval)
+            //一度だけ検出されればよいので、二度目以降の検出を無効にする処理を書く
+            const observer = new MutationObserver(() => {
+                clearTimeout(timer)
+                timer = setTimeout(async () => {
+                    await addElementForProgressBar();
+                    await addProgressBar();
+                }, 500);
+            });
+            observer.observe(target, {
+                childList: true,
+            });
+        };
+    }, 1000);
+    return
+};
 
 //プログレスバーを追加する
 async function addProgressBar() {
@@ -63,6 +91,7 @@ async function addProgressBar() {
     }, 1000);
 }
 
+//タイムオーバー時のクリックイベント処理
 function clickElement() {
     const isButton = document.querySelector('._1KqTg').parentElement.getAttribute("disabled");
     if (isButton == null) {
@@ -75,15 +104,19 @@ function clickElement() {
 async function main() {
     //await addElementForProgressBar();
     //await addProgressBar();
+    let count = 0;
     setInterval(async () => {
         if (await isSkillUrl() && !document.getElementById("container")) {
-            await addElementForProgressBar();
-            await addProgressBar();
-            console.log("test");
-        }
-
+            if (count == 0) {
+                await addElementForProgressBar();
+                await addProgressBar();
+                await isNextQuestion();
+                console.log("メイン関数");
+                count += 1;
+            };
+        } else { count = 0; };
     }, 1000);
-}
+};
 
 document.addEventListener("load",
     main()
