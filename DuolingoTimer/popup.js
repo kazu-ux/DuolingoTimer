e368379a96399627ed7abc8b08a7fe89222185e5
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-05-08 09:46:43 +0900

    #43

--- DuolingoTimer/popup.js
+++ DuolingoTimer/popup.js
@@ -1,8 +1,3 @@
-function main() {
-    console.log("test");
-}
-const setDefaultSeconds = () => { }
-
 //ボタンイベント
 const buttonEvent = (button) => {
     button.addEventListener("click", () => {
@@ -13,28 +8,30 @@ const buttonEvent = (button) => {
             setTimeout(() => {
                 document.querySelector("#seved").remove();
             }, 3000);
-
         } else {
             alert("1~600までの範囲で入力してください");
-        }
-    })
-}
+        };
+    });
+};
 
+//保存されている秒数をインプットに表示する
+//保存されていない場合はデフォルト値の60をセットする
+const displaySavedSeconds = () => {
+    chrome.storage.local.get("TimerSeconds", (seconds) => {
+        if (seconds.TimerSeconds) {
+            document.querySelector("input").value = Number(seconds.TimerSeconds);
+        } else {
+            document.querySelector("input").value = 60;
+        };
+    });
+};
 
+//保存ボタンが表示されるまで待機
 const interval = setInterval(() => {
     const button = document.querySelector("button");
     if (button) {
         clearInterval(interval);
         buttonEvent(button);
-        //保存されている秒数をインプットに表示する
-        chrome.storage.local.get("TimerSeconds", (seconds) => {
-            if (seconds.TimerSeconds) {
-                document.querySelector("input").value = Number(seconds.TimerSeconds);
-            } else {
-                document.querySelector("input").value = 60;
-            }
-
-            console.log(seconds.TimerSeconds);
-        });
-    }
+        displaySavedSeconds();
+    };
 }, 100);
